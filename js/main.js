1a87fc6aa5ce258da05ffb4f9cfa895a0cab5d24
avreeeb <burn0243@algonquinlive.com>
2015-03-16 22:28:11 -0400

    chewbacca

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ var $panel = $('.panel');
 var $tabs = $('.tabs');
 var $panels = $('.tab-panel');
 
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box')
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
 $menu.on('click', function () {
   $panel.toggleClass('js-panel-open');
 });
@@ -21,5 +30,22 @@ $tabs.on('click', 'a', function (e) {
 
   $tabs.find('.js-current').removeClass('js-current');
   $(this).addClass('js-current');
+});
+
+$btnShowHide.on('click', function () {
+    $box.toggleClass('js-box-dissappear');
+});
+
+$btnMove.on('click', function () {
+    $diamond.toggleClass('js-diamond-move');
+});
+
+$btnBounce.on('click', function () {
+    $circle.addClass('js-circle-bounce');
+});
+
+$circle.on('webkitAnimationEnd animationend', function () {
+	console.log('fired');
+    $circle.removeClass('js-circle-bounce');
 });
 	
